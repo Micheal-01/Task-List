4271e2e70393bbc5a0083a17179d51e18763551e
Tomisin <michealtomisin239@gmail.com>
2024-05-01 07:57:41 +0100

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('title', 'The list of tasks')
+
+@section('content')
+    {{-- <a href="{{ route('tasks.create') }}">Add Task</a> --}}
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}"
+          class="font-medium text-gray-700 underline decoration-pink-500">Add Task!</a>
+      </nav>
+    @forelse($tasks as $task)
+    <div>
+        {{-- <a href="{{ route('tasks.show', ['task' => $task->id]) }}"> {{ $task->title }}</a> --}}
+        <a href="{{ route('tasks.show', ['task' => $task->id]) }}"
+            @class(['line-through' => $task->completed])>{{ $task->title }}</a>
+    </div>
+        @empty
+    <div>There are no tasks</div>
+    @endforelse
+
+
+    @if ($tasks->count())
+    <nav>
+      {{ $tasks->links() }}
+    </nav>
+    @endif
+@endsection
